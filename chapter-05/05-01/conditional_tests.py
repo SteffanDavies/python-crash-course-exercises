d15d7af4a6db8d7f7024e727f4cb5dc75a8d5d40
Steffan Davies <50405969+SteffanDavies@users.noreply.github.com>
2022-12-27 10:24:53 +0000

    Chapter 5

---
+++ chapter-05/05-01/conditional_tests.py
@@ -0,0 +1,53 @@
+########################################################
+###   Programmers:    Steffan Davies                 ###
+###   Contact:        steffanclentdavies@gmail.com   ###
+###   Date:           26/12/2022                     ###
+########################################################
+
+# This script will demonstrate conditionals
+
+car = 'subaru'
+
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("Is car == 'audi'? I predict False.")
+print(car == 'audi')
+
+
+animal = 'dog'
+
+print("Is animal == 'dog'? I predict True.")
+print(animal == 'dog')
+
+print("Is animal == 'cat'? I predict False.")
+print(animal == 'cat')
+
+
+food = 'pizza'
+
+print("Is food == 'pizza'? I predict True.")
+print(food == 'pizza')
+
+print("Is food == 'salad'? I predict False.")
+print(food == 'salad')
+
+
+computer = 'linux'
+
+print("Is computer == 'linux'? I predict True.")
+print(computer == 'linux')
+
+print("Is computer == 'mac'? I predict False.")
+print(computer == 'mac')
+
+
+phone = 'android'
+
+print("Is phone == 'android'? I predict True.")
+print(phone == 'android')
+
+print("Is phone == 'ios'? I predict False.")
+print(phone == 'ios')
+
+
